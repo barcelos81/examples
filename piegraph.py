8a3b52ec951eee74cea75c96718f74d92a79da47
Adriano Barcelos <adrianobarcelos@gmail.com>
2021-01-05 20:15:45 -0300

    ajuste nos nomes das variáveis

--- piegraph.py
+++ piegraph.py
@@ -1,8 +1,8 @@
 from pylab import *
 
-produtos = ['Comédia', 'Romance', 'Ação', 'Terror', 'Drama']
-quantidade = [9, 15, 12, 6, 20]
+genero = ['Comédia', 'Romance', 'Ação', 'Terror', 'Drama']
+percentual = [9, 15, 12, 6, 20]
 cores = ['y', 'c', 'r', 'g', 'b']
 
-pie(quantidade, labels=produtos, colors=cores, autopct='%.1f%%')
+pie(percentual, labels=genero, colors=cores, autopct='%.1f%%')
 title('Gênero de filmes mais assistidos via streaming')
